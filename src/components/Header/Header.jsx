9819810897e0e251bf86cadf915120fe67ac52b5
Laureano Kronemberger <laureano.kronemberger@gmail.com>
2023-03-20 19:52:16 -0300

    comment Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,3 +18,14 @@ const Header = () => {
 }
 
 export default Header
+
+// Hi, i'm Laureano Kronemberger, Full Stack Web Developer.
+
+// I'm a Linux Enthusiast (I run Zorin OS on my personal laptop). In order to get a better grasp of my OS,
+// I have completed the following Youtube Course. It is pretty complete and helped me understand.
+// Curso Linux: https://youtu.be/wBp0Rb-ZJak
+
+// I have experience working as a developer, where my role included mantaining and adding functionalities to different 
+// systems, both in the Frontend and Backend. I also had to do some basic server administration.
+
+// Currently, I am studying to become a Network Administrator.
